a3089eee84ebbf06d7ef985fbd24fddf7efaefd6
PamelaCanaza <lunapamela886@gmail.com>
2025-10-31 02:41:54 -0400

    feat: agregar estilos a Home.tsx

--- appflor-fe/src/Home.tsx
+++ appflor-fe/src/Home.tsx
@@ -1,12 +1,15 @@
+import "./Home.css";
 function Home(props: any) {
   return (
-    <div>
-      <h1>Winter Bloom 🌸</h1>
-      <p>
-        Bienvenido a <b>Winter Bloom</b>, donde cada flor cuenta una historia.
-        Descubre arreglos únicos y la magia del arte floral.
-      </p>
-      <button onClick={props.onContinuar}>Entrar</button>
+    <div className="home-container">
+        <header className="home-header">
+            <h1>Winter Bloom 🌸</h1>
+            <p>
+                Bienvenido a <b>Winter Bloom</b>, donde cada flor cuenta una historia.
+                Descubre arreglos únicos y la magia del arte floral.
+            </p>
+            <button onClick={props.onContinuar}>Entrar</button>
+        </header>
     </div>
   );
 }

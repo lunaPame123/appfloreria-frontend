42f849f618623f831873a1782748c3a27a7085ee
PamelaCanaza <lunapamela886@gmail.com>
2025-10-31 03:24:41 -0400

    feat: agregar estilos al Navbar.tsx

--- appflor-fe/src/Navbar.tsx
+++ appflor-fe/src/Navbar.tsx
@@ -1,14 +1,16 @@
+import "./Navbar.css";
+
 function Navbar(props: any) {
   return (
-    <nav>
-      <div>🌸 Winter Bloom</div>
-      <div>
+    <nav className="navbar">
+      <div className="navbar-logo">🌸 Winter Bloom</div>
+      <div className="navbar-search">
         <input
           type="text"
           placeholder="Buscar flores, ramos o arreglos..."
         />
       </div>
-      <div>
+      <div className="navbar-actions">
         <button onClick={props.onHome}>Inicio</button>
         <button onClick={props.onLogin}>Iniciar Sesion</button>
       </div>
